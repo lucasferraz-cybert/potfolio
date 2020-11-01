9b6754f026f03669c8c98882548764f07db6f09f
lucas ferraz <silva.silva.ferraz@gmail.com>
2020-10-31 22:33:21 -0300

    primeiro commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# potfolio
-portifolio
---
+++ script.js
@@ -0,0 +1,5 @@
+var sitio = document.querySelector(".sitio");
+
+sitio.addEventListener("click", function(){
+  document.querySelector(".container").classList.toggle("show-menu");
+});
